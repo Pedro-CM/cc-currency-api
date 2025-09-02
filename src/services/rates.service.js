650556aff8f166bf4a69e0319fb6e58a945f1fb1
Pedro-CM <pedro.calderon.morales.35@gmail.com>
2025-09-01 20:33:32 -0600

    validaciones implementadas con joi

--- src/services/rates.service.js
+++ src/services/rates.service.js
@@ -2,11 +2,11 @@ import { Rate } from '../models/Rate.js';
 import { fetchFiatRates } from '../providers/openExchange.js';
 import { fetchCrypto } from '../providers/coingecko.js';
 import { BadRequest } from '@feathersjs/errors';
+import { rateSchema } from '../schemas/schemas.js';
 export class RatesService {
-    async find(params) {
-        const q = (params && params.query) || {};
+    async find(data) {
+        const q = (data && data.query) || {};
         const refresh = q.refresh === 'true';
-
         if (refresh) {
             const [fiat, crypto] = await Promise.all([
                 fetchFiatRates(),
@@ -32,33 +32,35 @@ export class RatesService {
     }
 
     async create(data) {
-        const base = (data.base || '').toUpperCase();
-        const symbol = String(data.symbol || '').toUpperCase();
+        const { error, value } = rateSchema.validate(data);
+        if (error) {
+            throw new BadRequest(error.details[0].message);
+        }
 
-        if (!symbol) throw new BadRequest('symbol es requerido');
-        if (!base) throw new BadRequest('base es requerido');
-        let value;
+        const { base, symbol } = value;
+
+        let rateValue;
 
         try {
-            const cryptoData = await fetchCrypto(base, symbol);
-            value = Number(cryptoData[symbol]);
+            const cryptoData = await fetchCrypto();
+
+            rateValue = Number(cryptoData[symbol]);
+
         } catch (cryptoError) {
             try {
                 const fiatData = await fetchFiatRates(base, symbol);
-                console.log(fiatData);
-                value = Number(fiatData[symbol]);
-                console.log(value);
+                rateValue = Number(fiatData[symbol]);
             } catch (fiatError) {
                 throw new BadRequest(`No se pudo obtener el valor para ${base} a ${symbol}.`);
             }
         }
-        if (!(value >= 0)) {
+        if (!(rateValue >= 0)) {
             throw new BadRequest('El valor de la tasa debe ser un número positivo.');
         }
 
         const doc = await Rate.findOneAndUpdate(
             { base, symbol },
-            { $set: { value } },
+            { $set: { value: rateValue } },
             { new: true, upsert: true }
         );
 
